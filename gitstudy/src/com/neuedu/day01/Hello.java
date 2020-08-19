5ea37d67e65c7cf2ec72d835485120fff32dd157
a958844394 <69879839+a958844394@users.noreply.github.com>
2020-08-19 16:30:15 +0800

    Update Hello.java

--- gitstudy/src/com/neuedu/day01/Hello.java
+++ gitstudy/src/com/neuedu/day01/Hello.java
@@ -3,6 +3,8 @@ package com.neuedu.day01;
 public class Hello {
 public static void main(String[] args) {
 		System.out.println("hello world");
+System.out.println("hello world2");
+System.out.println("hello world3");
 	}
   
 }
